2a094a85417342c2b53c39de97d6fc5ebbf3b556
Miguel Arenas <miguel.arenas@sitidoctor.com>
2021-07-15 11:23:44 -0500

    localstorage exercise

--- assets/js/cancion1.js
+++ assets/js/cancion1.js
@@ -1,15 +1,63 @@
-const audio = new Audio('https://dl.dropboxusercontent.com/s/w5211tlegof3vpo/y2mate.com_-_SIN_TI_SIN_MI_THE_MILLS_LETRA.mp3?dl=0');
-const btn = document.getElementById("play-button");
-const disc = document.getElementById("disc");
+const audio        = new Audio('https://dl.dropboxusercontent.com/s/mrdltmtuimoy3gf/mienteme.m4a?dl=0');
+const btn          = document.getElementById("play-button");
+const disc         = document.getElementById("disc");
+const formSong     = document.getElementById("formSong");
+const songName     = document.getElementById("songName");
+const songAlbum    = document.getElementById("songAlbum");
+const songYear     = document.getElementById("songYear");
+const songComposer = document.getElementById("songComposer");
+const songDuration = document.getElementById("songDuration");
+
+const song = localStorage.getItem( 'song' ) ? JSON.parse( localStorage.getItem( 'song' ) ) : null;
+
+if( song ) {
+    songName.innerHTML     = song.name;
+    songAlbum.innerHTML    = song.album;
+    songYear.innerHTML     = song.year;
+    songComposer.innerHTML = song.composer;
+    songDuration.innerHTML = song.duration;
+}
 
 btn.onclick = function() {
-    if (audio.paused) {
+    if( audio.paused ) {
         audio.play();
         disc.className = "disc-img turn";
 
     } else {
         audio.pause();
         disc.className = "disc-img";
+        
+    }
+}
+
+formSong.onsubmit = function( e ) {
+    e.preventDefault();
+
+    const formElements = formSong.elements;
+    let errs = "";
+
+    for( let input of formElements ) {
+        
+        const value = input.value.toLowerCase().trim();
+
+        if( input.id === "c2Name" && value !== "mienteme" ) errs += "el nombre de la cancion ";
+        if( input.id === "c2Album" && value !== "guadalupe" ) errs += "el nombre del album ";
+        if( input.id === "c2Year" && value !== "2011" ) errs += "el año de creación ";
+        if( input.id === "c2Composer" && value !== "alvaro charry" ) errs += "el nombre del compositor ";
+        if( input.id === "c2Duration" && value !== "03:03" ) errs += "la duracion de la cancion ";
 
     }
+
+    if( errs ) return alert("tienes los siguientes errores en " + errs);
+
+    const objSong = {
+        name     : "Miénteme",
+        album    : "Guadalupe",
+        year     : "2011",
+        composer : "Alvaro Charry",
+        duration : "03:03",
+    };
+
+    localStorage.setItem( "song", JSON.stringify( objSong ) );
+    location.reload();
 }
